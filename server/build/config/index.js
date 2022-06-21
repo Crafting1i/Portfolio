7d39aac731063b2d8d782789c2f450e260efae8f
Crafting1i <dauve228@gmail.com>
2022-06-22 02:39:04 +0300

    Deleted build

--- server/build/config/index.js
+++
@@ -1,8 +0,0 @@
-export const developmentConfig = {
-    port: 4000,
-    allowedOrigin: 'http://localhost:4000',
-};
-export const productionConfig = {
-    port: 4000,
-    allowedOrigin: 'http://localhost:4000',
-};
